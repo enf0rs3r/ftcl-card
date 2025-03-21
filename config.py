542c2ec6e6de7d9b5b25be22f09821479798d9f9
enf0rs3r <sea.0.lucky@gmail.com>
2025-03-21 15:37:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Токен бота, полученный через BotFather
-TOKEN = "7936215498:AAEK5400_9HM1CMfYBYQiKw893UfMxnUSTQ"  # Заменить на свой токен
+TOKEN = "7936215498:AAE432BQFdYDBNRxuEeWlbZNzEX8zUeJupk"  # Заменить на свой токен
 
 # Путь к базе данных (для SQLite)
 DATABASE_PATH = os.path.join(os.path.dirname(__file__), "database", "cards.db")
